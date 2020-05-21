7015b8fe7bf1ace0ad72229cc9a9366d905efbbc
Kristy <kristy.huang15@gmail.com>
2020-05-21 00:22:35 -0400

    js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
 $(document).ready(function(){
+  //toggler btn
+  $('.navbar-toggler').click(function(){
+    $('.navbar-toggler').toggleClass('change')
+  });
+
+  //sticky navbar
+  $(window).scroll(function(){
+    let position = $(this).scrollTop();
+    if (position >= 55.8) {
+      $('.navbar').addClass('fixed-top');
+    } else {
+      $('.navbar').removeClass('fixed-top');
+    }
+  });
+
+  //smooth scroll
+  /*$('.nav-item a, #back-to-top').click(function(link) {
+    link.preventDefault();
+    let target = $(this).attr('href');
+
+    $('html, body').stop.animate({
+      scrollTop: $(target).offset().top - 25
+    }, 3000);
+  })*/
+  
     //ripples
-    $("header, .info").ripples({
+    /*$("header, .info").ripples({
         dropRadius: 25,
         perturbance: 0.6,
-      });
+      });*/
 });
