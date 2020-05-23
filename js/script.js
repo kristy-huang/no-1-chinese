2202c406e14aa9a37d815a655f6fcd0d6c2d1127
Kristy <kristy.huang15@gmail.com>
2020-05-22 22:32:40 -0400

    menu

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
   //sticky navbar
   $(window).scroll(function(){
     let position = $(this).scrollTop();
-    if (position >= 2) {
+    if (position >= 10) {
       $('.navbar').addClass('navbar-background');
       $('.navbar').addClass('fixed-top');
     } else {
